3a0948f895f10488dc95d12131545d0515d94715
Linh Nguyen <linh.nguyen2@asiantech.vn>
2019-09-28 16:58:10 +0700

    change placeholder for input

--- src/components/Feature/TodoList/TodoList.vue
+++ src/components/Feature/TodoList/TodoList.vue
@@ -2,7 +2,7 @@
   <div class="todo-list-wrapper">
     <form class="form-todo">
       <div class="form-group">
-        <input class="add-task-input" type="text" placeholder="Add task...">
+        <input class="add-task-input" type="text" placeholder="What needs to be done?">
         <button class="btn btn-primary btn-add">
           <i class="icofont-plus"></i>
         </button>
