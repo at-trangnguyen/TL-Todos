36ac0952f057994dfac7865fea95d98158277ce5
Linh Nguyen <linh.nguyen2@asiantech.vn>
2019-09-28 16:47:25 +0700

    create add task input

--- src/components/Feature/TodoList/TodoList.vue
+++ src/components/Feature/TodoList/TodoList.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="todo-list-wrapper">
-    <p>TodoList is works!</p>
+    <form class="form-todo">
+      <div class="form-group">
+        <input class="add-task-input" type="text" placeholder="Add task...">
+        <button class="btn btn-primary btn-add">
+          <i class="icofont-plus"></i>
+        </button>
+      </div>
+    </form>
     <todo-item/>
   </div>
 </template>
