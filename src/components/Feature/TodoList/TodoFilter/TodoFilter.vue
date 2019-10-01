616fb727d5d8a0ea78eb7c6a7f0d70f9d89d05cf
Linh Nguyen <linh.nguyen2@asiantech.vn>
2019-10-01 11:00:00 +0700

    split form add todo & todo filter

---
+++ src/components/Feature/TodoList/TodoFilter/TodoFilter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="todo-container">
+    <ul class="tabs">
+      <li class="tab-item">
+        <a class="active" href="#">All tasks</a>
+      </li>
+      <li class="tab-item">
+        <a href="#">Active</a>
+      </li>
+      <li class="tab-item">
+        <a href="#">Complete</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'todo-filter'
+  };
+</script>
