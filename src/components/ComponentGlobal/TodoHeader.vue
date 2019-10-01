a54575e64e708e99a017a9c89902dc4486a70043
Linh Nguyen <linh.nguyen2@asiantech.vn>
2019-10-01 10:39:22 +0700

    create todo header component

---
+++ src/components/ComponentGlobal/TodoHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <div class="page-wrapper"> -->
+    <header>
+      <div class="logo-wrapper">
+        <img class="logo-image" src="../../assets/images/logo_icon.png" alt="logo">
+        <h1 class="page-title">Todoist</h1>
+      </div>
+    </header>
+    <!-- <todo-list/> -->
+  <!-- </div> -->
+</template>
+
+<script>
+  export default {
+    name: 'todo-header'
+  };
+</script>
