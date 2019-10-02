3a9b014ddc3ecd2c0058bdcbc67d5bbcbf513dfc
Linh Nguyen <linh.nguyen2@asiantech.vn>
2019-10-02 11:14:36 +0700

    change todo status

--- src/components/Feature/TodoList/TodoItem/TodoItem.vue
+++ src/components/Feature/TodoList/TodoItem/TodoItem.vue
@@ -1,9 +1,9 @@
 <template>
   <li class="todo-item">
     <div class="todo-info">
-      <input id="checkbox" type="checkbox" class="checkbox">
-      <label class="lb-checkbox" for="checkbox"></label>
-      <span class="task-name">Create Todo</span>
+      <input :id="todo.id" type="checkbox" class="checkbox" @change="changeStatus(todo.status)" :checked="todo.status === 'completed'">
+      <label class="lb-checkbox" :for="todo.id"></label>
+      <span class="task-name">{{ todo.task }}</span>
     </div>
     <button class="btn btn-no-style btn-remove">
       <i class="icofont-close"></i>
@@ -13,6 +13,17 @@
 
 <script>
   export default {
-    name: 'todo-item'
+    name: 'todo-item',
+    props: ['todo'],
+    methods: {
+      changeStatus(status) {
+        if (status === 'active') {
+          this.todo.status = 'completed';
+        } else {
+          this.todo.status = 'active';
+        }
+        this.$emit("inputData", this.todo);
+      }
+    }
   };
 </script>
--- src/components/Feature/TodoList/TodoList.vue
+++ src/components/Feature/TodoList/TodoList.vue
@@ -3,7 +3,7 @@
     <form-add-todo/>
     <todo-filter/>
     <ul class="todos">
-      <todo-item/>
+      <todo-item v-for="(todo, index) in todos" :key="index" :todo="todo" @inputData="updateTodo"/>
     </ul>
   </div>
 </template>
@@ -18,6 +18,36 @@
       FormAddTodo,
       TodoFilter,
       TodoItem
+    },
+    data() {
+      return {
+        todos: [
+          {
+            id: 1,
+            task: 'Create Todo PWA with Vue',
+            status: 'active'
+          },
+          {
+            id: 2,
+            task: 'Configure Progressive Web App',
+            status: 'completed'
+          },
+          {
+            id: 3,
+            task: 'Configure webpack',
+            status: 'active'
+          }
+        ]
+      }
+    },
+    methods: {
+      updateTodo(todo) {
+        for (let i = 0; i < this.todos.length; i++) {
+          if (this.todos[i].id === todo.id) {
+            this.todos[i].task = todo.task;
+          }
+        }
+      }
     }
   };
 </script>
